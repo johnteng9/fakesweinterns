6c853c3ac4a255a1ff860782d607037c7cfc8d61
Dnalyaw <waylandla@gmail.com>
2023-11-17 17:31:00 -0800

    added navigation bar

---
+++ frontend/vite-project/src/navbar.jsx
@@ -0,0 +1,21 @@
+export default function Navbar() {
+    return (
+      <>
+    <nav id="navbar">
+    <label class="logo" id="logo">Cal<strong>Central</strong></label>
+      <ul class="navbar-links">
+          <a><li>John</li>
+          <br></br>
+          <li>Admin</li></a>
+      </ul>
+    </nav>
+    <div id="menu">
+      <ul>
+        <a><li>View Classes</li></a>
+        <a><li>Class Search and Enroll</li></a>
+        <a><li>Schedule Planner</li></a>
+      </ul>
+    </div>
+  </>
+    )
+}
