e0be14fef6421a6b7d7371588242ec3bfa64da69
YaBoiPasta27 <148606567+YaBoiPasta27@users.noreply.github.com>
2023-12-03 23:05:53 -0800

    Login.jsx
    added database integration
    username: test1@berkeley.edu
    password:123456

--- frontend/vite-project/src/Login.jsx
+++ frontend/vite-project/src/Login.jsx
@@ -1,74 +1,78 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import './index.css'
 
-const User = {
-  email: '123',
-  pw: '123'
-}
+const LoginPage = () => {
+  const navigate = useNavigate();
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(null);
 
-
-export default function Login() {
-    const navigate = useNavigate();
-
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const [notAllow, setNotAllow] = useState(true);
-   
-    
-    const onClickConfirmButton = () => {
-      
-      if(username === User.email && password === User.pw) {
-        alert('Successfully Login.')
-        navigate('/home')
+  const handleLogin = async () => {
+    console.log(username);
+    console.log(password);
+    try {
+      const apiUrl = `http://localhost:3003/login?email=${username}&password=${password}`; // Replace with the actual login API endpoint
+      console.log(`http://localhost:3003/login?email=${username}&password=${password}`);
+      const response = await fetch(apiUrl, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          username,
+          password,
+        }),
+      });
+      console.log('querysuccess');
+      if (response.ok) {
+        navigate('/home');
+        console.log('Login successful');
       } else {
-        alert("Incorrect.");
+        // Handle login error
+        const errorData = await response.json();
+        setError(`Error: ${response.status} - ${errorData.message}`);
       }
+    } catch (error) {
+      // Handle network errors
+      setError(`An error occurred: ${error.message}`);
     }
-    const handleInputChange = () => {
-    setNotAllow(!username || !password);
-    };
-    
+  };
 
-    return (
-      <div className='fakebody'>
-        <div className="page">
-          <div className="contentWrap">
-            <div>
-            <h2 className="title">
-              <strong>CalNet </strong> 
-              Authentication Service
-              </h2>
-              <form method='post' id='loginForm'>
-                <fieldset>
-                  <p>
-                    <label htmlFor="username" id="username-label">CalNet ID: </label>
-                    <input className="required" id="username" size="25" type="text" autoComplete='off' autoCapitalize='none' aria-labelledby='username-label' name="username" value={username}
-                    onChange={(e) => {
-                      setUsername(e.target.value);
-                      handleInputChange();
-                    }}></input>
-                  </p>
-                  <p>
-                    <label htmlFor="password">Passphrase (Case Sensitive): </label>
-                    <input className="required" id="password" size="25" type="text" autoComplete='off' name="password" value={password}
-                    onChange={(e) => {
-                      setPassword(e.target.value);
-                      handleInputChange();
-                    }}></input>
-                  </p>
 
-                </fieldset>
+  return (
+    <div className='fakebody'>
+      <div className="page">
+        <div className="contentWrap">
+          <div>
+          <h2 className="title">
+            <strong>CalNet </strong> 
+            Authentication Service
+            </h2>
+            <form method='post' id='loginForm'>
+              <fieldset>
+                <p>
+                  <label htmlFor="username" id="username-label">CalNet ID: </label>
+                  <input className="required" id="username" size="25" type="text" autoComplete='off' autoCapitalize='none' aria-labelledby='username-label' name="username" value={username}
+                  onChange={(e) => setUsername(e.target.value)}></input>
+                </p>
+                <p>
+                  <label htmlFor="password">Passphrase (Case Sensitive): </label>
+                  <input className="required" id="password" size="25" type="text" autoComplete='off' name="password" value={password}
+                  onChange={(e) => setPassword(e.target.value)}></input>
+                </p>
 
-              </form>
-    
-            <button onClick={onClickConfirmButton} disabled={notAllow} className="bottomButton">
-              Login
-            </button>
-            </div>
+              </fieldset>
+
+            </form>
+  
+          <button onClick={handleLogin} className="bottomButton">
+            Login
+          </button>
           </div>
         </div>
       </div>
-    );
+    </div>
+  );
 }
-
+export default LoginPage;
