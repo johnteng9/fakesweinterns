ef772de0228cac0808f8ca7952c9ee17b56ffc8e
YaBoiPasta27 <148606567+YaBoiPasta27@users.noreply.github.com>
2023-12-05 21:09:17 -0800

    Update class_description.jsx

--- frontend/vite-project/src/class_description.jsx
+++ frontend/vite-project/src/class_description.jsx
@@ -1,11 +1,38 @@
-export default function Description({className, classNumber,description, days, time}) {
+import React, { useState, useEffect } from 'react';
+export default function ClassDescription({classID}) {
+    const [data, setData] = useState([]);
+  const classNumber = classID;
+  const apiLink = `http://localhost:3004/getClass?id=${classNumber}`;
+  console.log(apiLink);
+  useEffect(() => {
+    // Function to fetch data from the server
+    const fetchData = async () => {
+      try {
+        // Replace 'your-api-endpoint' with the actual API endpoint for your SQL database
+        const response = await fetch(apiLink);
+        
+        // Check if the response is successful (status code 200)
+        if (response.ok) {
+          const result = await response.json();
+          setData(result);
+        } else {
+          console.error('Failed to fetch data');
+        }
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    // Call the fetchData function when the component mounts
+    fetchData();
+  }, []); // The empty dependency array ensures that the effect runs once when the component mounts
     return (
         <div id="description">
-            <h1>{classNumber}</h1>
-            <h3>{className}</h3>
-            <p>{description}</p>
+            <h1>{data.classNumber}</h1>
+            <h3>{data.name}</h3>
+            <p>{data.description}</p>
             <div>
-                <h6 id="days">{days} @ {time}</h6>
+                <h6 id="days">{data.days} @ {data.start}</h6>
             </div>
         </div>
     )
